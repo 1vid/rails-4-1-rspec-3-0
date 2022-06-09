256640021c6eebeb33b18b6d60b3bb38607cfb72
Velichko Ilia <velichko.ilia@gmail.com>
2022-06-09 16:12:56 +0300

    Chapter 4: Factory_Girl(Bot) & Faker

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -1,6 +1,14 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :contact do
+    firstname { Faker::Name.first_name }
+    lastname { Faker::Name.last_name}
+    email { Faker::Internet.email }
+
+    after(:build) do |contact| #callback we can also use before(:build), before(:create), and after(:create)
+      [:home_phone, :work_phone, :mobile_phone].each do |phone|
+        contact.phones << build(:phone,
+          phone_type: phone, contact: contact)
+      end
+    end
   end
 end
---
+++ spec/factories/phones.rb
@@ -0,0 +1,18 @@
+FactoryGirl.define do
+  factory :phone do
+    association :contact  #create a new Contact on the fly for this phone
+    phone { Faker::PhoneNumber.phone_number }
+
+    factory :home_phone do
+      phone_type 'home'
+    end
+
+    factory :work_phone do
+      phone_type 'work'
+    end
+
+    factory :mobile_phone do
+      phone_type 'mobile'
+    end
+  end
+end
